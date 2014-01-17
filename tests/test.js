0df498c2c500379fa602a71b063fbb54503d98f7
zaphod1984 <zaphod84@gmx.de>
2014-01-17 10:32:54 +0100

    break all the tests!

--- tests/test.js
+++ tests/test.js
@@ -10,7 +10,7 @@ describe('Chainr', function() {
             .seq(function(cb) {
                 order.push('a');
 
-                setTimeout(cb, 100);
+                setTimeout(cb, 200);
             })
             .seq(function(cb) {
                 order.push('b');
@@ -20,7 +20,7 @@ describe('Chainr', function() {
             .seq(function(cb) {
                 order.push('c');
 
-                expect(order).to.deep.equal(['a', 'b']);
+                expect(order).to.deep.equal(['a', 'b', 'c']);
                 done();
             });
     });
@@ -54,6 +54,74 @@ describe('Chainr', function() {
                 order.push('last');
 
                 expect(order).to.deep.equal(['c', 'b', 'a', 'last']);
+                done();
+            });
+    });
+
+    it('should add the supplied values to the chain.vars object', function(done) {
+        var chain = chainr();
+
+        chain.seq('foo', function(cb) {
+            cb(null, 'bar');
+        });
+
+        chain.seq('bax', function(cb) {
+            cb(null, 'baz');
+        });
+
+        chain.seq(function() {
+            expect(chain.vars).to.deep.equal({
+                foo: 'bar',
+                bax: 'baz'
+            });
+
+            done();
+        })
+    });
+
+    it('should not call catch when error occurs', function(done) {
+        chainr()
+            .seq(function(cb) {
+                setTimeout(cb, 100);
+            })
+            .seq(function(cb) {
+                setTimeout(cb, 100);
+            })
+            .catch(function() {
+                expect(false).to.be.true;
+
+                done();
+            })
+            .seq(function() {
+                expect(true).to.be.true;
+
+                done();
+            });
+    });
+
+    it('should jump to catch when error occurs, should execute subsequential seq, should not intermediate', function(done) {
+        var caboomError = new Error('caboom');
+        var order = [];
+
+        chainr()
+            .seq(function(cb) {
+                setTimeout(function() {
+                    order.push('a');
+
+                    cb(caboomError)
+                }, 100);
+            })
+            .seq(function(cb) {
+                order.push('b');
+
+                setTimeout(cb, 100);
+            })
+            .catch(function(error, cb) {
+                order.push(error);
+                cb();
+            })
+            .seq(function() {
+                expect(order).to.deep.equal(['a', caboomError]);
 
                 done();
             });
