104493212c73649001c64809807ae197427c3681
zaphod1984 <zaphod84@gmx.de>
2014-01-17 10:22:19 +0100

    break all the tests

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Chainr
 
-<!--
 [![Build Status](https://travis-ci.org/zaphod1984/chainr.png)](https://travis-ci.org/zaphod1984/chainr)
 
+<!--
 [![NPM](https://nodei.co/npm/chainr.png)](https://nodei.co/npm/chainr/)
 
 [![NPM](https://nodei.co/npm-dl/chainr.png?months=3)](https://nodei.co/npm/chainr/)
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,61 @@
 var expect = require('chai').expect;
 
+var chainr = require('../');
+
 describe('Chainr', function() {
-    it('should', function() {
-        expect(true).to.be.true;
+    it('should call in sequence', function(done) {
+        var order = [];
+
+        chainr()
+            .seq(function(cb) {
+                order.push('a');
+
+                setTimeout(cb, 100);
+            })
+            .seq(function(cb) {
+                order.push('b');
+
+                setTimeout(cb, 100);
+            })
+            .seq(function(cb) {
+                order.push('c');
+
+                expect(order).to.deep.equal(['a', 'b']);
+                done();
+            });
+    });
+
+    it('should call in parallel', function(done) {
+        var order = [];
+
+        chainr()
+            .par(function(cb) {
+                setTimeout(function() {
+                    order.push('a');
+
+                    cb();
+                }, 200);
+            })
+            .par(function(cb) {
+                setTimeout(function() {
+                    order.push('b');
+
+                    cb();
+                }, 100);
+            })
+            .par(function(cb) {
+                setTimeout(function() {
+                    order.push('c');
+
+                    cb();
+                }, 50);
+            })
+            .seq(function() {
+                order.push('last');
+
+                expect(order).to.deep.equal(['c', 'b', 'a', 'last']);
+
+                done();
+            });
     });
 });
