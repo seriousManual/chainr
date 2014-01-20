be9899d0c4812731f3d18093d7fa6982c3431e3f
zaphod1984 <zaphod84@gmx.de>
2014-01-20 20:33:46 +0100

    added examples

---
+++ example/doubleCall.js
@@ -0,0 +1,18 @@
+var chainr = require('../');
+
+chainr()
+    .seq(function (cb) {
+        console.log('1');
+        setTimeout(function() {
+            cb();
+            cb();
+        }, 100);
+    })
+    .seq(function (cb) {
+        console.log('2');
+        setTimeout(cb, 100);
+    })
+    .seq(function (cb) {
+        console.log('3');
+        setTimeout(cb, 100);
+    });
---
+++ example/throw.js
@@ -0,0 +1,17 @@
+var chainr = require('../');
+
+chainr()
+    .seq(function (cb) {
+        console.log('1');
+        setTimeout(function() {
+            cb(new Error('foo'));
+        }, 100);
+    })
+    .seq(function (cb) {
+        console.log('2');
+        setTimeout(cb, 100);
+    })
+    .seq(function (cb) {
+        console.log('3');
+        setTimeout(cb, 100);
+    });
